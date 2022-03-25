34a53983959304a3c15a3d58a4a737640bbe523f
NetVPS <vpssmr@gmail.com>
2022-03-25 16:31:54 -0600

    Update SPR.sh

--- LINKS-LIBRERIAS/SPR.sh
+++ LINKS-LIBRERIAS/SPR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Soporte Remoto (SPR)
 echo "" >/dev/null 2>&1
-wget https://www.dropbox.com/s/b9uktqipi8vu8ww/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+wget https://raw.githubusercontent.com/NetVPS/VPS-MX_Oficial/master/LINKS-LIBRERIAS/HELP.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
 chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
