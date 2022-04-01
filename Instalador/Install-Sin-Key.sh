be1eb29ea7b2b71c8ed2044425f0269878294c9d
NetVPS <vpssmr@gmail.com>
2022-04-01 17:46:08 -0600

    Update Install-Sin-Key.sh

--- Instalador/Install-Sin-Key.sh
+++ Instalador/Install-Sin-Key.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 clear && clear
-cd $HOME
-RutaBin="/bin"
+rm -rf /etc/localtime &>/dev/null
+ln -s /usr/share/zoneinfo/America/Argentina/Tucuman /etc/localtime &>/dev/null
+
 apt install net-tools -y &>/dev/null
 myip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1)
 myint=$(ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}')
@@ -25,6 +26,8 @@ msg() {
   -azu) cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
   -verd) cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
   -bra) cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -nazu) cor="${COLOR[6]}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
+  -gri)cor="\e[5m\033[1;100m" && echo -ne "${cor}${2}${SEMCOR}";;
   "-bar2" | "-bar") cor="${VERMELHO}————————————————————————————————————————————————————" && echo -e "${SEMCOR}${cor}${SEMCOR}" ;;
   esac
 }
@@ -50,127 +53,182 @@ fun_bar() {
   echo -e " \033[1;33m[\033[1;31m########################################\033[1;33m] - \033[1;32m100%\033[0m"
   sleep 1s
 }
-install_paketes() {
-  clear && clear
-  ### PAQUETES PRINCIPALES
-  msg -bar2
-  echo -e " \e[5m\033[1;100m   =====>> ►► 🐲 SCRIPT - VPS•MX  🐲 ◄◄ <<=====   \033[1;37m"
-  msg -bar2
-  msg -ama "   PREPARANDO INSTALACION | VERSION: $vesaoSCT"
-  msg -bar
-  #grep
-  apt-get install netcat -y &>/dev/null
-  apt-get install netpipes -y &>/dev/null
-  apt-get install grep -y &>/dev/null
-  apt-get install net-tools -y &>/dev/null
-  sudo add-apt-repository universe &>/dev/null
-  sudo apt-get install netcat-traditional -y &>/dev/null
-  sudo add-apt-repository main -y &>/dev/null
-  sudo add-apt-repository universe -y &>/dev/null
-  sudo add-apt-repository restricted -y &>/dev/null
-  sudo add-apt-repository multiverse -y &>/dev/null
-  sudo apt-get install software-properties-common -y &>/dev/null
-  sudo add-apt-repository ppa:neurobin/ppa -y &>/dev/null
-  sudo apt-get install build-essential -y &>/dev/null
-  apt-get install shc &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "grep" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "grep" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install grep............ $ESTATUS "
-  #gawk
-  apt-get install gawk -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "gawk" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "gawk" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install gawk............ $ESTATUS "
-  #mlocate
-  apt-get install mlocate -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "mlocate" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "mlocate" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install mlocate......... $ESTATUS "
-  #lolcat gem
-  apt-get install lolcat -y &>/dev/null
-  sudo gem install lolcat &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "lolcat" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "lolcat" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install lolcat.......... $ESTATUS "
-  #at
-  [[ $(dpkg --get-selections | grep -w "at" | head -1) ]] || apt-get install at -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "at" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "at" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install at.............. $ESTATUS "
-  #nano
-  [[ $(dpkg --get-selections | grep -w "nano" | head -1) ]] || apt-get install nano -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "nano" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "nano" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install nano............ $ESTATUS "
-  #bc
-  [[ $(dpkg --get-selections | grep -w "bc" | head -1) ]] || apt-get install bc -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "bc" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "bc" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  systemedia &>/dev/null
-  echo -e "\033[97m    # apt-get install bc.............. $ESTATUS "
-  #lsof
-  [[ $(dpkg --get-selections | grep -w "lsof" | head -1) ]] || apt-get install lsof -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "lsof" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "lsof" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install lsof............ $ESTATUS "
-  #figlet
-  [[ $(dpkg --get-selections | grep -w "figlet" | head -1) ]] || apt-get install figlet -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "figlet" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "figlet" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install figlet.......... $ESTATUS "
-  #cowsay
-  [[ $(dpkg --get-selections | grep -w "cowsay" | head -1) ]] || apt-get install cowsay -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "cowsay" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "cowsay" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install cowsay.......... $ESTATUS "
-  #screen
-  [[ $(dpkg --get-selections | grep -w "screen" | head -1) ]] || apt-get install screen -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "screen" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "screen" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install screen.......... $ESTATUS "
-  #python
-  [[ $(dpkg --get-selections | grep -w "python" | head -1) ]] || apt-get install python -y &>/dev/null
-  apt-get install python -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install python.......... $ESTATUS "
-  #python3
-  [[ $(dpkg --get-selections | grep -w "python3" | head -1) ]] || apt-get install python3 -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python3" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python3" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install python3......... $ESTATUS "
-  #python3-pip
-  [[ $(dpkg --get-selections | grep -w "python3-pip" | head -1) ]] || apt-get install python3-pip -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python3-pip" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "python3-pip" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install python3-pip..... $ESTATUS "
-  #ufw
-  [[ $(dpkg --get-selections | grep -w "ufw" | head -1) ]] || apt-get install ufw -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "ufw" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "ufw" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install ufw............. $ESTATUS "
-  #unzip
-  [[ $(dpkg --get-selections | grep -w "unzip" | head -1) ]] || apt-get install unzip -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "unzip" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "unzip" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install unzip........... $ESTATUS "
-  #zip
-  [[ $(dpkg --get-selections | grep -w "zip" | head -1) ]] || apt-get install zip -y &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "zip" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "zip" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install zip............. $ESTATUS "
-  #apache2
-  apt-get install apache2 -y &>/dev/null
-  sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf >/dev/null 2>&1
-  service apache2 restart >/dev/null 2>&1
-  [[ $(dpkg --get-selections | grep -w "apache2" | head -1) ]] || ESTATUS=$(echo -e "\033[91mFALLO DE INSTALACION") &>/dev/null
-  [[ $(dpkg --get-selections | grep -w "apache2" | head -1) ]] && ESTATUS=$(echo -e "\033[92mINSTALADO") &>/dev/null
-  echo -e "\033[97m    # apt-get install apache2......... $ESTATUS "
 
+print_center() {
+  if [[ -z $2 ]]; then
+    text="$1"
+  else
+    col="$1"
+    text="$2"
+  fi
+
+  while read line; do
+    unset space
+    x=$(((54 - ${#line}) / 2))
+    for ((i = 0; i < $x; i++)); do
+      space+=' '
+    done
+    space+="$line"
+    if [[ -z $2 ]]; then
+      msg -azu "$space"
+    else
+      msg "$col" "$space"
+    fi
+  done <<<$(echo -e "$text")
 }
-install_paketes
+
+title() {
+  clear
+  msg -bar
+  if [[ -z $2 ]]; then
+    print_center -azu "$1"
+  else
+    print_center "$1" "$2"
+  fi
+  msg -bar
+}
+
+stop_install() {
+  title "INSTALACION CANCELADA"
+  exit
+}
+
+time_reboot() {
+  print_center -ama "REINICIANDO VPS EN $1 SEGUNDOS"
+  REBOOT_TIMEOUT="$1"
+
+  while [ $REBOOT_TIMEOUT -gt 0 ]; do
+    print_center -ne "-$REBOOT_TIMEOUT-\r"
+    sleep 1
+    : $((REBOOT_TIMEOUT--))
+  done
+  reboot
+}
+
+os_system() {
+  system=$(cat -n /etc/issue | grep 1 | cut -d ' ' -f6,7,8 | sed 's/1//' | sed 's/      //')
+  distro=$(echo "$system" | awk '{print $1}')
+
+  case $distro in
+  Debian) vercion=$(echo $system | awk '{print $3}' | cut -d '.' -f1) ;;
+  Ubuntu) vercion=$(echo $system | awk '{print $2}' | cut -d '.' -f1,2) ;;
+  esac
+}
+
+repo() {
+  link="https://raw.githubusercontent.com/NetVPS/Multi-Script/main/Source-List/$1.list"
+  case $1 in
+  8 | 9 | 10 | 11 | 16.04 | 18.04 | 20.04 | 20.10 | 21.04 | 21.10 | 22.04) wget -O /etc/apt/sources.list ${link} &>/dev/null ;;
+  esac
+}
+
+ dependencias(){
+ 	soft="sudo bsdmainutils zip unzip ufw curl python python3 python3-pip openssl screen cron iptables lsof nano at mlocate gawk grep bc jq curl npm nodejs socat netcat netcat-traditional net-tools cowsay figlet lolcat"
+ 
+ 	for i in $soft; do
+ 		leng="${#i}"
+ 		puntos=$(( 21 - $leng))
+ 		pts="."
+ 		for (( a = 0; a < $puntos; a++ )); do
+ 			pts+="."
+ 		done
+ 		msg -nazu "    Instalando $i$(msg -ama "$pts")"
+ 		if apt install $i -y &>/dev/null ; then
+ 			msg -verd " INSTALADO"
+ 		else
+ 			msg -verm2 " ERROR"
+ 			sleep 2
+ 			tput cuu1 && tput dl1
+ 			print_center -ama "aplicando fix a $i"
+ 			dpkg --configure -a &>/dev/null
+ 			sleep 2
+ 			tput cuu1 && tput dl1
+ 
+ 			msg -nazu "    Instalando $i$(msg -ama "$pts")"
+ 			if apt install $i -y &>/dev/null ; then
+ 				msg -verd " INSTALADO"
+ 			else
+ 				msg -verm2 " ERROR"
+ 			fi
+ 		fi
+ 	done
+ }
+
+post_reboot() {
+  echo 'wget -O /root/install.sh "https://raw.githubusercontent.com/NetVPS/VPS-MX_Oficial/master/Instalador/Install-Sin-Key.sh"; clear; sleep 2; chmod +x /root/install.sh; /root/install.sh --continue' >>/root/.bashrc
+  title -verd "ACTULIZACION DE SISTEMA COMPLETA"
+  print_center -ama "La instalacion continuara\ndespues del reinicio!!!"
+  msg -bar
+}
+
+install_start() {
+  msg -bar
+
+  echo -e "\e[1;97m           \e[5m\033[1;100m   ACTULIZACION DE SISTEMA   \033[1;37m"
+  msg -bar
+  print_center -ama "Se actualizaran los paquetes del sistema.\n Puede demorar y pedir algunas confirmaciones.\n"
+  msg -bar3
+  msg -ne "\n Desea continuar? [S/N]: "
+  read opcion
+  [[ "$opcion" != @(s|S) ]] && stop_install
+  clear && clear
+  msg -bar
+  echo -e "\e[1;97m           \e[5m\033[1;100m   ACTULIZACION DE SISTEMA   \033[1;37m"
+  msg -bar
+  os_system
+  repo "${vercion}"
+  apt update -y
+  apt upgrade -y
+}
+
+ install_continue(){
+   os_system
+   msg -bar
+   echo -e "      \e[5m\033[1;100m   COMPLETANDO PAQUETES PARA EL SCRIPT   \033[1;37m"
+   msg -bar
+   print_center -ama "$distro $vercion"
+   print_center -verd "INSTALANDO DEPENDENCIAS"
+   msg -bar3
+   dependencias
+   msg -bar3
+   print_center -azu "Removiendo paquetes obsoletos"
+   apt autoremove -y &>/dev/null
+   sleep 2
+   tput cuu1 && tput dl1
+   msg -bar
+   print_center -ama "Si algunas de las dependencias fallo!!!\nal terminar, puede intentar instalar\nla misma manualmente usando el siguiente comando\napt install nom_del_paquete"
+   msg -bar
+   read -t 60 -n 1 -rsp $'\033[1;39m       << Presiona enter para Continuar >>\n'
+ }
+
+
+while :; do
+  case $1 in
+  -s | --start) install_start && post_reboot && time_reboot "15" ;;
+  -c | --continue)
+    #rm /root/Install-Sin-Key.sh &>/dev/null
+    sed -i '/Instalador/d' /root/.bashrc
+    install_continue
+    break
+    ;;
+  # -u | --update)
+  #   install_start
+  #   install_continue
+  #   break
+  # ;;
+  *) exit ;;
+  esac
+done
+
+clear && clear
+msg -bar2
+echo -e " \e[5m\033[1;100m   =====>> ►► 🐲 MULTI - SCRIPT  🐲 ◄◄ <<=====   \033[1;37m"
+msg -bar2
+print_center -ama "LISTADO DE SCRIPT DISPONIBLES"
+msg -bar
+
 #-BASH SOPORTE ONLINE
-wget https://www.dropbox.com/s/gt8g3y8ol4nj4hf/SPR.sh -O /usr/bin/SPR > /dev/null 2>&1
+wget https://www.dropbox.com/s/gt8g3y8ol4nj4hf/SPR.sh -O /usr/bin/SPR >/dev/null 2>&1
 chmod +x /usr/bin/SPR
 install_oficial() {
 
@@ -185,7 +243,7 @@ install_oficial() {
   rm -rf /etc/VPS-MX/MEUIPvps
 }
 install_mod() {
-    mkdir /etc/VPS-MX >/dev/null 2>&1
+  mkdir /etc/VPS-MX >/dev/null 2>&1
   cd /etc
   wget https://raw.githubusercontent.com/NetVPS/VPS-MX_Oficial/master/SCRIPT-v8.5x%20Mod/VPS-MX.tar.xz >/dev/null 2>&1
   tar -xf VPS-MX.tar.xz >/dev/null 2>&1
@@ -197,7 +255,7 @@ install_mod() {
 }
 
 msg -bar
-echo -e "\e[1;93m          CUAL VPS-MX DESEA INSTALAR "
+echo -e "\e[1;93m          CUAL SCRIPT DESEA INSTALAR "
 msg -bar
 echo -ne " \e[1;93m [\e[1;32m1\e[1;93m]\033[1;31m > \e[1;97m INSTALAR 8.5 OFICIAL \e[97m \n"
 echo -ne " \e[1;93m [\e[1;32m2\e[1;93m]\033[1;31m > \033[1;97m INSTALAR 8.5x MOD \e[97m \n"
